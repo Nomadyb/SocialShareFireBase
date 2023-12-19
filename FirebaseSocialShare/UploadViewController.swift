2402a95638f399d76dc9ba904e41fafb3e20f11c
Nomadyb <51696895+Nomadyb@users.noreply.github.com>
2023-12-20 00:45:07 +0300

    Add files via upload
    starter

--- FirebaseSocialShare/UploadViewController.swift
+++ FirebaseSocialShare/UploadViewController.swift
@@ -88,8 +88,24 @@ class UploadViewController: UIViewController,UIImagePickerControllerDelegate,UIN
 							
 							//DB
 							
+							let firestoreDatabase = Firestore.firestore()
+							var firestoreReference : DocumentReference? = nil
 							
-						}
+							
+							let firestorePost = ["imageUrl": imageUrl!, "postedBy": Auth.auth().currentUser!.email!,"postComment":self.commentText.text!,"date":"date","likes":0] as [String : Any ]
+							
+							firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
+								if error != nil {
+									self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+									
+									
+									
+								}
+								
+								})
+							
+							
+							}
 					}
 					
 				}
